04e60baf5be58147e816a229ffae53514042bb56
ColeHud <cmhudson11@gmail.com>
2015-09-12 19:51:01 -0400

    improved redirect

--- Router/server.js
+++ Router/server.js
@@ -1,9 +1,17 @@
-//cole
+//Cole
 var express = require('express');
 var app = express();
 var numberOfPlayers = 0;
 
-//redirect to the local server
-app.get('/', function(req, res) {
-  res.redirect("http://mhackslocal.colehudson.net/" + numberOfPlayers);
+app.get('/', function (req, res) {
+	numberOfPlayers++;
+ 	res.redirect("http://mhackslocal.colehudson.net/" + numberOfPlayers);
+});
+
+var server = app.listen(3000, function () 
+{
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('Example app listening at http://%s:%s', host, port);
 });
