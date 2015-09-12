0fbf12fdc083b7863c8067f57a1d43f489a57d57
ColeHud <cmhudson11@gmail.com>
2015-09-12 14:53:05 -0400

    did some stuff

--- Web-Server/server.js
+++ Web-Server/server.js
@@ -1,5 +1,9 @@
 /*
-Cole
+Copyright (C) 2015 Electronic Arts Inc.  All rights reserved.
+ 
+This software is solely licensed pursuant to the Hackathon License Agreement,
+Available at:  www.eapathfinders.com/license
+All other use is strictly prohibited. 
 */
 
 require('log-timestamp');
