711ecfe6f3784fcadc47ff5d74a0e8da02c392fc
ColeHud <cmhudson11@gmail.com>
2015-09-12 19:47:41 -0400

    improved redirect

--- Router/server.js
+++ Router/server.js
@@ -1,26 +1,9 @@
-//Cole
-
-//Lets require/import the HTTP module
-var http = require('http');
-
+//cole
+var express = require('express');
+var app = express();
 var numberOfPlayers = 0;
 
-//Lets define a port we want to listen to
-const PORT=8088; 
-
-//We need a function which handles requests and send response
-function handleRequest(request, response)
-{
-	numberOfPlayers++;
-    response.redirect("http://mhacks.colehudson.net/" + numberOfPlayers);
-}
-
-//Create a server
-var server = http.createServer(handleRequest);
-
-//Lets start our server
-server.listen(PORT, function()
-{
-    //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT);
+//redirect to the local server
+app.get('/', function(req, res) {
+  res.redirect("http://mhackslocal.colehudson.net/" + numberOfPlayers);
 });
